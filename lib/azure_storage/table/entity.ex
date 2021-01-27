1f6929c4774315d08391c0f703312909955eb298
Sokun Chorn <chornsokun@gmail.com>
2021-01-27 23:15:51 +1100

    manual impl Jason.Encoder

--- lib/azure_storage/table/entity.ex
+++ lib/azure_storage/table/entity.ex
@@ -1,5 +1,11 @@
 defmodule AzureStorage.Table.Entity do
-  # defstruct _: "", "$": ""
+  defstruct _: "", "$": ""
 
   def new(value, type), do: %{_: value, "$": type}
+end
+
+defimpl Jason.Encoder, for: AzureStorage.Table.Entity do
+  def encode(%AzureStorage.Table.Entity{} = entity, _opts) do
+    %{_: Map.get(entity, :_, ""), "$": Map.get(entity, :"$")} |> Jason.encode!()
+  end
 end
