f56ec27311dcbc24e6471c0fe8c85dd801489abe
Sokun Chorn <chornsokun@gmail.com>
2021-03-29 23:29:23 +1100

    remove unused modules

--- lib/azure_storage/behavior/query_stringify.ex
+++
@@ -1,5 +0,0 @@
-defmodule Az.QueryStringify do
-  @typep options :: Keyword.t()
-
-  @callback to_query_string(options) :: {:ok, String.t()}
-end
--- lib/azure_storage/core/uri.ex
+++
@@ -1,6 +0,0 @@
-defmodule AzureStorage.Core.Uri do
-  defstruct ~w"uri canonical_resource"a
-
-  def new(uri) when is_binary(uri) do
-  end
-end
