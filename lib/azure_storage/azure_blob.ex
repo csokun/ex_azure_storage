f4f37aa25675f7b88617e06498515758cef684d2
beng <ben@kineviz.com>
2024-08-19 02:14:31 -0400

    enable pagination of list_blobs

--- lib/azure_storage/azure_blob.ex
+++ lib/azure_storage/azure_blob.ex
@@ -83,24 +83,32 @@ defmodule AzureStorage.Blob do
           | {:error, String.t()}
   def list_blobs(%Context{service: "blob"} = context, container, options \\ []) do
     {:ok, opts} = NimbleOptions.validate(options, Schema.list_blobs_options())
-    max_results = opts[:max_results]
 
-    prefix =
-      case String.length(opts[:prefix]) do
-        0 -> []
-        _ -> ["&prefix=", opts[:prefix]]
+    max_results =
+      case opts[:maxresults] do
+        nil -> ""
+        _ -> "&maxresults=#{opts[:maxresults]}"
       end
 
-    query =
-      ([
-         container,
-         "?restype=container",
-         container,
-         "&comp=list",
-         "&maxresults=",
-         max_results
-       ] ++ prefix)
-      |> IO.iodata_to_binary()
+    prefix =
+      case opts[:prefix] do
+        nil -> ""
+        _ -> "&prefix=#{opts[:prefix]}"
+      end
+
+    delimiter =
+      case opts[:delimiter] do
+        nil -> ""
+        _ -> "&delimiter=#{opts[:delimiter]}"
+      end
+
+    marker =
+      case opts[:marker] do
+        nil -> ""
+        _ -> "&marker=#{opts[:marker]}"
+      end
+
+    query = "#{container}?restype=container&comp=list#{max_results}#{prefix}#{delimiter}#{marker}"
 
     context
     |> build(method: :get, path: query)
@@ -157,8 +165,8 @@ defmodule AzureStorage.Blob do
   ```
   {:ok, context} = AzureStorage.create_blob_service("account_name", "account_key")
   context |> put_blob("blobs",
-    "cache-key-1.json", 
-    "{\\"data\\": []}", 
+    "cache-key-1.json",
+    "{\\"data\\": []}",
     content_type: "application/json;charset=\\"utf-8\\""
   )
   ```
@@ -309,7 +317,7 @@ defmodule AzureStorage.Blob do
 
     path =
       case String.starts_with?(opts[:path], "/") do
-        true -> String.slice(opts[:path], 1..-1)
+        true -> String.slice(opts[:path], 1..-1//-1)
         false -> opts[:path]
       end
 
---
+++ test/list_blobs_test.exs
@@ -0,0 +1,30 @@
+defmodule TestExAzureStorage do
+  use ExUnit.Case
+  alias AzureStorage.Blob
+
+  @account_name Application.compile_env(:ex_azure_storage, :account_name, "")
+  @account_key Application.compile_env(:ex_azure_storage, :account_key, "")
+  @container "tmp"
+
+  setup_all do
+    {:ok, context} = AzureStorage.create_blob_service(@account_name, @account_key)
+    %{context: context}
+  end
+
+  test "can paginate", %{context: context} do
+    first_page = Blob.list_blobs(context, @container, maxresults: 1)
+    {:ok, %{items: %{
+      "Name" => name,
+      "Properties" => properties
+    }, marker: marker}} = first_page
+    assert is_binary(name)
+    assert is_map(properties)
+    second_page = Blob.list_blobs(context, @container, marker: marker, maxresults: 1)
+    {:ok, %{items: %{
+      "Name" => name,
+      "Properties" => properties
+    }}} = second_page
+    assert is_binary(name)
+    assert is_map(properties)
+  end
+end
